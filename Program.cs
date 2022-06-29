7bc5e32cf3323535ee0925bb0b771ef270faff52
Akhil Sompalli <aksompalli@gmail.com>
2022-06-29 11:13:32 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 var url = $"http://0.0.0.0:{port}";
-var target = Environment.GetEnvironmentVariable("TARGET") ?? "World V1.2";
+var target = Environment.GetEnvironmentVariable("TARGET") ?? "World V1.2 !!!";
 
 var app = builder.Build();
 
